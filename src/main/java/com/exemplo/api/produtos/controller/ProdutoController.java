03b1844f1e8554ded9ce1f476277abd9ac220767
Baumzin <brenosuvale@gmail.com>
2025-09-01 19:44:54 -0300

    correção da atividade diante o PDF

--- src/main/java/com/exemplo/api/produtos/controller/ProdutoController.java
+++ src/main/java/com/exemplo/api/produtos/controller/ProdutoController.java
@@ -14,7 +14,7 @@ import com.exemplo.api.produtos.model.Produto;
 import com.exemplo.api.produtos.repository.ProdutoRepository;
 
 @RestController
-@RequestMapping("/produtos")
+@RequestMapping("/")
 public class ProdutoController {
     private final ProdutoRepository repository;
 
